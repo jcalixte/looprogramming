e14405a40edd44b223c0dc2e5bf2ec9122ea2fef
Julien Calixte <julienc@bam.tech>
2023-04-07 14:16:25 +0200

    sort imports

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { ViteSSG } from 'vite-ssg'
-import generatedRoutes from 'virtual:generated-pages'
-import { setupLayouts } from 'virtual:generated-layouts'
-import App from './App.vue'
+import { setupLayouts } from "virtual:generated-layouts"
+import generatedRoutes from "virtual:generated-pages"
+import { ViteSSG } from "vite-ssg"
+import App from "./App.vue"
 
-import '@unocss/reset/tailwind.css'
-import './styles/main.css'
-import 'uno.css'
+import "@unocss/reset/tailwind.css"
+import "uno.css"
+import "./styles/main.css"
 
 const routes = setupLayouts(generatedRoutes)
 
@@ -15,6 +15,8 @@ export const createApp = ViteSSG(
   { routes, base: import.meta.env.BASE_URL },
   (ctx) => {
     // install all modules under `modules/`
-    Object.values(import.meta.globEager('./modules/*.ts')).forEach(i => i.install?.(ctx))
-  },
+    Object.values(import.meta.globEager("./modules/*.ts")).forEach((i) =>
+      i.install?.(ctx)
+    )
+  }
 )
